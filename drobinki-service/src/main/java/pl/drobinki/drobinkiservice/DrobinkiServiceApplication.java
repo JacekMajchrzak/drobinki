d4f41a61c3ed4ece97e6f3c2f8979cfb4ba38254
Jacek Majchrzak <jacek.majchrzak@contractors.roche.com>
2019-07-25 11:42:57 +0200

    first commit

---
+++ drobinki-service/src/main/java/pl/drobinki/drobinkiservice/DrobinkiServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.drobinki.drobinkiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrobinkiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrobinkiServiceApplication.class, args);
+	}
+
+}
